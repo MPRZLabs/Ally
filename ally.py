6f3b733eb8ba4062f98373755e56f7455ec91e52
Michał Sidor (Michcioperz) <michcioperz@autistici.org>
2014-03-09 13:48:12 +0100

    now building .html files and serving

--- ally.py
+++ ally.py
@@ -22,27 +22,29 @@
 #  
 #  
 
-import sys, mpi, os, string, logging
+import sys, mpi, os, string, logging, SimpleHTTPServer, SocketServer
 
 logging.basicConfig(level=logging.DEBUG)
 
 def parse(f):
+    ret = ""
     logging.debug("Parsing %s" % f)
     for line in open(f, "r"):
         if line.startswith("start"):
-            print(m.start())
+            ret=ret+m.start()
         if line.startswith("tytuł "):
             m.title(line[7:len(line)-1])
         if line.startswith("góra"):
-            print(m.header())
+            ret=ret+m.header()
         if line.startswith("koniec"):
-            print(m.end())
+            ret=ret+m.end()
         if line.startswith("import "):
-            print(m.include(line[7:len(line)-1]))
+            ret=ret+m.include(line[7:len(line)-1])
         if line.startswith("import! "):
-            print(m.include(line[8:len(line)-1]), True)
+            ret=ret+m.include(line[8:len(line)-1], True)
         if line.startswith("wiersz "):
-            print(m.poem(line[7:len(line)-1]))
+            ret=ret+m.poem(line[7:len(line)-1])
+    return ret
 
 def printhelp():
     print "You need to specify filename as an argument"
@@ -50,20 +52,31 @@ def printhelp():
 def main():
     global m
     if len(sys.argv) < 2:
-        printhelp()
+        print("Specify action")
         return 1
+    if len(sys.argv) < 3:
+        path = "."
     else:
-        if os.path.isdir(sys.argv[1]):
-            cfgpth = string.rstrip(sys.argv[1], "/") + "/" + ".allyconfig"
+        path = sys.argv[2]
+    if sys.argv[1] == "build" or sys.argv[1] == "serve":
+        if os.path.isdir(path):
+            cfgpth = string.rstrip(path, "/") + "/" + ".allyconfig"
             if os.path.isfile(cfgpth):
                 m = mpi.MPi(cfgpth)
                 for page in m.pages:
-                    parse(m.path[0:len(m.path)-12] + "/" + page + ".ally")
+                    with open(m.path[0:len(m.path)-12] + "/" + page + ".html", "w") as target:
+                        target.write(parse(m.path[0:len(m.path)-12] + "/" + page + ".ally"))
             else:
                 logging.error("Config file is not a file: %s" % cfgpth)
                 return 3
         else:
             logging.error("Not a directory")
             return 2
+    if sys.argv[1] == "serve":
+        PORT = 8000
+        Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+        httpd = SocketServer.TCPServer(("", PORT), Handler)
+        print "serving at port", PORT
+        httpd.serve_forever()
 if __name__ == '__main__':
     main()
